a0821af54d4466131ef005642d774f8ab4cf4a86
Josh Prewitt <joshua.prewitt@ni.com>
2020-09-03 21:48:55 -0500

    code clean up

--- update-package-repository.py
+++ update-package-repository.py
@@ -24,14 +24,35 @@ def job_status(job_id):
         response = requests.get(job_url, headers=configuration.api_key, verify=False)
         status = json.loads(response.text)["jobs"][0]["status"]
         if status == "SUCCEEDED":
-            print('SUCCEEDED = ', response.text)
             break
         if status == "FAILED":
-            print('FAILED = ', response.text)
             break      
         print(status)
         time.sleep(1)
     return response
+
+def check_for_updates(feed_id):
+    check_url = host+'/v1/feeds/'+feed_id+'/update-check'
+    response = requests.post(check_url, headers=configuration.api_key, verify=False)
+    job_id = json.loads(response.text)["jobId"]
+    print("JOB ID = ",job_id)
+    status = job_status(job_id)
+    return status
+
+def get_updates(update_id):
+    update_url =  host+'/v1/updates/'+update_id+dc()
+    response = requests.get(update_url, headers=configuration.api_key, verify=False)
+    updates = response.text
+    return updates
+
+def update_feed(feed_id, updates):
+    update_url =  host+'/v1/feeds/'+feed_id+'/update-apply?ignoreImportErrors=false&shouldOverwrite=false'
+    headers = {'Content-type': 'application/json'}
+    headers.update(configuration.api_key)
+    response = requests.post(update_url, verify=False, data=updates, headers=headers)
+    job_id = json.loads(response.text)["jobId"]
+    status = job_status(job_id)
+    return status
 
 def main():
     # updates all of the feeds in the package repository based on the feeds they were replicated from
@@ -43,39 +64,26 @@ def main():
     feed_ids = []
     for feed in feeds["feeds"]:
         feed_ids.append(feed["id"])
-        
-    print("Feed IDs = ",feed_ids)
+    
+    print("FEED IDS = ",feed_ids)   
 
     # check for updates
     # todo: hard code feed ID for now, but will utlimately iterate over every feed
-    feed_id = '5f518c1ba9460dbc70490640'
-    check_url = host+'/v1/feeds/'+feed_id+'/update-check'
-    response = requests.post(check_url, headers=configuration.api_key, verify=False)
-    job_id = json.loads(response.text)["jobId"]
-    print("Job ID = ",job_id)
-
-    status = job_status(job_id)
+    feed_id = '5f518c1ba9460dbc70490640'        
+    status = check_for_updates(feed_id)
+    print('CHECK STATUS = ', status.text)
     update_id = json.loads(status.text)["jobs"][0]["resourceId"]
 
-    # if the feed wasn't updated after 10 seconds move to the next feed
-    #if status != "SUCCEEDED"
-    #    break
+    # todo: continue if there are no updates or the job failed
 
     # get list of updates
-    update_url =  host+'/v1/updates/'+update_id+dc()
-    response = requests.get(update_url, headers=configuration.api_key, verify=False)
-    updates = response.text
-    print('Updates = ', response.text)
+    updates = get_updates(update_id)
+    print('UPDATES = ', updates)
 
     # apply updates
-    update_url =  host+'/v1/feeds/'+feed_id+'/update-apply?ignoreImportErrors=false&shouldOverwrite=false'
-    headers = {'Content-type': 'application/json'}
-    headers.update(configuration.api_key)
-    response = requests.post(update_url, verify=False, data=updates, headers=headers)
-    job_id = json.loads(response.text)["jobId"]
-    print('Update response = ',response.text)
+    status = update_feed(feed_id,updates)
+    print('APPLY STATUS = ', status.text)
 
-    status = job_status(job_id)
 
 if __name__ == "__main__":
     main()
