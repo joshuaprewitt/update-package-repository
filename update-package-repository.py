fd2ed5fa28ee6d85946f0a3c5b9621bc258951c6
Josh Prewitt <joshua.prewitt@ni.com>
2020-09-03 20:52:00 -0500

    refactor

--- update-package-repository.py
+++ update-package-repository.py
@@ -7,13 +7,31 @@ import urllib3
 import time
 import random
 
+def job_status(job_id):
+    # wait for the job to complete
+    for i in range(10):
+        # create a random number to avoid getting cached server results
+        rand = str(random.randrange(100000))
+        job_url =  host+'/v1/jobs?_dc='+rand+'&id='+job_id
+        response = requests.get(job_url, headers=configuration.api_key, verify=False)
+        status = json.loads(response.text)["jobs"][0]["status"]
+        if status == "SUCCEEDED":
+            print('SUCCEEDED = ', response.text)
+            update_id = json.loads(response.text)["jobs"][0]["resourceId"]
+            break
+        if status == "FAILED":
+            print('FAILED = ', response.text)
+            break      
+        print(status)
+        time.sleep(1)
+    return response
+
 # disable https warnings
 urllib3.disable_warnings()
 
 # load SystemLink HTTP configuration 
 configuration = systemlink.clientconfig.get_configuration('nirepo')
 host = configuration.host
-print(configuration.api_key)
  
 # get feed ids
 feeds_url = host+"/v1/feeds"
@@ -33,22 +51,8 @@ response = requests.post(check_url, headers=configuration.api_key, verify=False)
 job_id = json.loads(response.text)["jobId"]
 print("job_id = ",job_id)
 
-# wait for the job to complete
-for i in range(10):
-    # create a random number to avoid getting cached server results
-    rand = str(random.randrange(100000))
-    job_url =  host+'/v1/jobs?_dc='+rand+'&id='+job_id
-    response = requests.get(job_url, headers=configuration.api_key, verify=False)
-    status = json.loads(response.text)["jobs"][0]["status"]
-    if status == "SUCCEEDED":
-        print('SUCCEEDED = ',response.text)
-        update_id = json.loads(response.text)["jobs"][0]["resourceId"]
-        break
-    if status == "FAILED":
-        print('FAILED = ', response.text)
-        break     
-    print(status)
-    time.sleep(1)
+status = job_status(job_id)
+update_id = json.loads(status.text)["jobs"][0]["resourceId"]
 
 # if the feed wasn't updated after 10 seconds move to the next feed
 #if status != "SUCCEEDED"
@@ -66,24 +70,12 @@ rand = str(random.randrange(100000))
 update_url =  host+'/v1/feeds/'+feed_id+'/update-apply?ignoreImportErrors=false&shouldOverwrite=false'
 headers = {'Content-type': 'application/json'}
 headers.update(configuration.api_key)
-print(headers)
 response = requests.post(update_url, verify=False, data=updates, headers=headers)
 job_id = json.loads(response.text)["jobId"]
 print('update response = ',response.text)
 
-# wait for the job to complete
-for i in range(10):
-    # create a random number to avoid getting cached server results
-    rand = str(random.randrange(100000))
-    job_url =  host+'/v1/jobs?_dc='+rand+'&id='+job_id
-    response = requests.get(job_url, headers=configuration.api_key, verify=False)
-    status = json.loads(response.text)["jobs"][0]["status"]
-    if status == "SUCCEEDED":
-        print('SUCCEEDED = ', response.text)
-        update_id = json.loads(response.text)["jobs"][0]["resourceId"]
-        break
-    if status == "FAILED":
-        print('FAILED = ', response.text)
-        break      
-    print(status)
-    time.sleep(1)
+status = job_status(job_id)
+
+
+
+
